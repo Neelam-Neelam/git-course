31dde8a32c2cf4aa22691aaacb077cd1a0c8e594
Neelam-Neelam <prasanthnm01@gmail.com>
2024-06-20 16:24:47 +0530

    "movie rating service"

---
+++ movie-data-service/src/test/java/io/javabrains/movie_data_service/MovieDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.movie_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
